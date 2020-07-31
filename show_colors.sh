101e4787ee5e153b65a2de8d1fd21015c9412020
robocopAlpha <35454738+robocopAlpha@users.noreply.github.com>
2020-07-31 18:16:08 +0300

    add theme files and screenshots

---
+++ show_colors.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Syntax of colors from https: //misc.flogisoft.com/bash/tip_colors_and_formatting
+
+NC='\033[0m'
+
+# Colors
+echo -e ""
+echo -e "    \033[39mRegular${NC}   \033[1;39mBold${NC}     \033[30;107mBackground${NC}    \033[30;107mLighter BG${NC}"
+echo -e "30: \033[30mBlack${NC}     \033[1;30mBlack${NC}"
+echo -e "31: \033[31mRed${NC}       \033[1;31mRed${NC}         \033[41mRed${NC}           \033[101mRed${NC}"
+echo -e "32: \033[32mGreen${NC}     \033[1;32mGreen${NC}       \033[42mGreen${NC}         \033[102mgreen${NC}"
+echo -e "33: \033[33mYellow${NC}    \033[1;33mYellow${NC}      \033[43mYellow${NC}        \033[103mYellow${NC}"
+echo -e "34: \033[34mBlue${NC}      \033[1;34mBlue${NC}        \033[44mBlue${NC}          \033[104mBlue${NC}"
+echo -e "35: \033[35mMagenta${NC}   \033[1;35mMagenta${NC}     \033[45mMagenta${NC}       \033[105mMagenta${NC}"
+echo -e "36: \033[36mCyan${NC}      \033[1;36mCyan${NC}        \033[46mCyan${NC}          \033[106mCyan${NC}"
+echo -e "37: \033[37mGray${NC}      \033[1;37mGray${NC}        \033[47mGray${NC}          \033[100mGray${NC}"
+echo -e "39: \033[39mWhite${NC}     \033[1;39mWhite${NC}       \033[107mWhite${NC}         \033[107mWhite${NC}"
+echo -e ""
